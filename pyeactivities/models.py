3952bd80a176e8f9408ceedb76f89a7d9f541190
Fraser May <frasertmay@gmail.com>
2019-02-18 19:00:09 +0000

    Added basic models for CSP, member, and committee member

---
+++ pyeactivities/models.py
@@ -0,0 +1,70 @@
+class CSP:
+    """Represents a CSP as returned by the CSP endpoint"""
+
+    def __init__(
+        self,
+        code: str,
+        name: str,
+        web_name: str,
+        acronym: str,
+    ):
+        self.code = code
+        self.name = name
+        self.web_name = web_name
+        self.acronym = acronym
+
+    def __repr__(self):
+        return "CSP(code='{code}', name='{name}')".format(
+            code=self.code, name=self.name
+        )
+
+
+class Member:
+    def __init__(
+        self,
+        first_name: str,
+        surname: str,
+        cid: str,
+        email: str,
+        login: str,
+        order_no: str,
+        member_type: str,
+    ):
+        self.first_name = first_name
+        self.surname = surname
+        self.cid = cid
+        self.email = email
+        self.login = login
+        self.order_no = order_no
+        self.member_type = member_type
+
+
+class CommitteeMember:
+    """Represents a committee members as returned by the committee member endpoint"""
+
+    def __init__(
+        self,
+        first_name: str,
+        surname: str,
+        cid: str,
+        email: str,
+        login: str,
+        post_name: str,
+        phone_no: str,
+        start_date: str,
+        end_date: str,
+    ):
+        self.first_name = first_name
+        self.surname = surname
+        self.cid = cid
+        self.email = email
+        self.login = login
+        self.post_name = post_name
+        self.phone_no = phone_no
+        self.start_date = start_date
+        self.end_date = end_date
+
+    def __repr__(self):
+        return "CommitteeMember(surname='{sn}', first_name='{fn}', cid={cid})".format(
+            sn=self.surname, fn=self.first_name, cid=self.cid
+        )
