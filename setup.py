689ecf3cf26d0536685759c393c2c3d3ef8e0bae
Fraser May <frasertmay@gmail.com>
2019-04-16 10:42:05 +0100

    Updated setup.py to use setuptools and added LICENSE

--- setup.py
+++ setup.py
@@ -1,13 +1,24 @@
-from distutils.core import setup
+import setuptools
 
-setup(
-    name="pyeactivities",
-    packages=["pyeactivities"],
+with open("README.rst", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="pyeactivities-ftm",
     version="0.1.0",
-    description="Python API Wrapper for Imperial College Union eActivities",
     author="Fraser May",
     author_email="frasertmay@gmail.com",
+    description="Python API Wrapper for Imperial College Union eActivities",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
     url="https://github.com/ftm/pyeactivities",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+    ],
     install_requires=[
         "requests>=2.21.0"
     ]
